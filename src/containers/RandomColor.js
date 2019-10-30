ff4f5fd3f2dc161b8058755d6263cb9197b94898
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2019-10-30 15:32:22 -0700

    beginning tests

--- src/containers/RandomColor.js
+++ src/containers/RandomColor.js
@@ -6,7 +6,7 @@ export default class RandomColor extends Component {
   
   pickColor = (colors) => {
     let newBackground = colors[Math.floor(Math.random() * colors.length)];
-    if(this.state.backgroundColor === newBackground) this.setState({ backgroundImg: 'url(https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg)' })
+    if(this.state.backgroundColor === newBackground) this.setState({ backgroundImg: 'url(https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg)' });
     else this.setState({ backgroundColor: newBackground, backgroundImg: '' });
   };
 
