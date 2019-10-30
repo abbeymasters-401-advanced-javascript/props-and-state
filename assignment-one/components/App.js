1a5b3776197a53cdc69766dd915b18295f794068
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2019-10-30 14:10:11 -0700

    adding in second assignment

---
+++ assignment-one/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ColorContainer from '../src/containers/ColorContainerner';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <ColorContainer />
+    );
+  }
+}
