d9474ebfd224805f449720ec77689cddf5e84cc2
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2019-10-30 13:53:23 -0700

    working on extracting state

--- src/containers/ColorContainer.js
+++ src/containers/ColorContainer.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
+import ColorPicker from '../components/colors/ColorPicker';
+import ColorDisplay from '../components/colors/ColorDisplay';
+
 
 export default class ColorContainer extends Component {
-  
   state = {
     backgroundColor: 'black'
   };
-  
+
   selectColor = backgroundColor => {
     this.setState({ backgroundColor });
   }
 
-  render(
+  render() {
+    const colors = ['red', 'green', 'black', 'yellow'];
+    const { backgroundColor } = this.state;
 
-  )
+    return (
+      <>
+        <ColorDisplay backgroundColor={backgroundColor} />
+        <ColorPicker
+          selectColor={this.selectColor}
+          colors={colors} />
+      </>
+    );
+  }
 }
 
 
